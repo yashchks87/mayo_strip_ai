2e58b438a7a0ccde53fbfd4132017b01b89ee7cf
yashchks87 <yashchks87@gmail.com>
2023-07-26 16:14:56 +0000

    Shell script is fixed.

--- start.sh
+++ start.sh
@@ -7,4 +7,6 @@ kaggle competitions download -c mayo-clinic-strip-ai
 
 unzip mayo-clinic-strip-ai.zip
 
+rm mayo-clinic-strip-ai.zip
+
 python scale_images.py -d ../../files/train/ -s ../../files/resized_train/ -i 256 -p ../issue_images.pickle
